5503122640a11d0648295689f7d78f96b07d7eb2
danujs <86118475+danujs@users.noreply.github.com>
2023-09-26 05:50:23 +0700

    Update super.py

--- super.py
+++ super.py
@@ -5,7 +5,6 @@ class Racer:
 		self.name = name
 
 
-
 	def getName(self):
 		return self.name 
 
@@ -15,7 +14,6 @@ class Racer:
 
 class ItalianRider(Racer):
 
-	#when we use super(), we don't need to use the self parameter
 
 	def __init__(self, name):        #to make automatic, we create function __init__
 		# Racer.__init__(self, name) #to call __init__ Racer from parent then it run well
@@ -33,8 +31,4 @@ print(Rider.getName() + " is " + Rider.getSkill())
 
 
 # Description
-"""Each child class that inherit attributes from parent
-it can replace certain attributes from that parent"""
-"""we can see that Italian and Spain Rider have their own
-attributes, while Malaysian inherit the attributes 'getSkill'
-from it parent"""
+"""when we use super(), we don't need to use the self parameter"""
